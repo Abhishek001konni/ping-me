97c0cdb748029fe6901f1d95593074896b2938e1
Abhishek001konni <abhishek001konni@gmail.com>
2025-08-27 13:19:04 -0400

    feat(client): add form submission handler and improve login/signup state transitions

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -9,6 +9,15 @@ const LoginPage = () => {
   const [bio, setBio] = useState("");
   const [isDataSubmitted, setIsDataSubmitted] = useState(false);
 
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    if (currState === "Sign up" && !isDataSubmitted) {
+      setIsDataSubmitted(true);
+      return;
+    }
+  };
+
   return (
     <div
       className="min-h-screen bg-cover bg-center flex items-center justify-center
@@ -18,12 +27,20 @@ const LoginPage = () => {
       <img src={assets.logo_big} alt="" className="w-[min(30vw,250px)]" />
       {/* Right Section */}
       <form
+        onSubmit={onSubmitHandler}
         className="border-2 bg-white/8 text-white border-gray-500 p-6 flex
       flex-col gap-6 rounded-lg shadow-lg"
       >
         <h2 className="font-medium text-2xl flex justify-between items-center">
           {currState}
-          <img src={assets.arrow_icon} alt="" className="w-5 cursor-pointer" />
+          {isDataSubmitted && (
+            <img
+              onClick={() => setIsDataSubmitted(false)}
+              src={assets.arrow_icon}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          )}
         </h2>
         {currState === "Sign up" && !isDataSubmitted && (
           <input
@@ -87,15 +104,24 @@ const LoginPage = () => {
         <div className="flex flex-col gap-2">
           {currState === "Sign up" ? (
             <p className="text-sm text-gray-600">
-              Already have an account?{" "}
-              <span className="font-medium text-violet-500 cursor-pointer">
+              Already have an account?
+              <span
+                onClick={() => {
+                  setCurrState("Login");
+                  setIsDataSubmitted(false);
+                }}
+                className="font-medium text-violet-500 cursor-pointer"
+              >
                 Login
               </span>
             </p>
           ) : (
             <p className="text-sm text-gray-600">
-              Create an account{" "}
-              <span className="font-medium text-violet-500 cursor-pointer">
+              Create an account
+              <span
+                onClick={() => setCurrState("Sign up")}
+                className="font-medium text-violet-500 cursor-pointer"
+              >
                 Click here
               </span>
             </p>
