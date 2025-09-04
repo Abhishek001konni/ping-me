a8ab9e77907caf5d467cd5b8fa9d1b4f4448db23
Abhishek001konni <abhishek001konni@gmail.com>
2025-09-04 15:37:53 -0400

    feat(server): add sendMessage controller and update route imports

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,4 +1,5 @@
-import Message from "../models/Message";
+import cloudinary from "../lib/cloudinary.js";
+import Message from "../models/Message.js";
 
 // Get all users except the current user
 export const getUsersForSidebar = async (req, res) => {
@@ -61,6 +62,32 @@ export const markMessagesAsSeen = async (req, res) => {
     const { id } = req.params;
     await Message.findByIdAndUpdate(id, { seen: true });
     res.json({ success: true });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Send message to selected user
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const receiverId = req.params.id;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    res.json({ success: true, newMessage });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
