ee675c0147c25227e3d46b353c8c40f054ba28a3
Abhishek001konni <abhishek001konni@gmail.com>
2025-09-04 14:10:42 -0400

    feat(server): add user routes and Cloudinary config

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./config/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -11,7 +12,9 @@ const server = http.createServer(app);
 app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
+// Routes
 app.use("/api/status", (req, res) => res.send("Server is running"));
+app.use("/api/auth", userRouter)
 
 // MongoDb 
 await connectDB();
